f36668ca78baf298f192e16326a049bf45e1419e
andriyha <andriy.halyasovskyy@playtech.com>
2017-08-25 10:35:07 +0300

    Added support for UC Browser EN on iOS

---
+++ src/browser-ui-state/state-providers/ucbrowser-en-ios-state-provider.js
@@ -0,0 +1,19 @@
+import StateProvider from './state-provider'
+
+export default class UCBrowserENiOSStateProvider extends StateProvider {
+    constructor(screenObj, windowObj) {
+        let thresholds = {
+            landscape : {
+                collapsed: 30.3,
+                keyboard: 50.4,
+            },
+            portrait : {
+                collapsed: 12.95,
+                keyboard: 26.3
+            }
+        }
+
+
+        super(screenObj, windowObj, thresholds)
+    }
+}
