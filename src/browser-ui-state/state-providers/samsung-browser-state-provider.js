f1dedc895d5f62a71c8262ed4a042fb2ef636574
andriyha <andriy.halyasovskyy@playtech.com>
2017-08-28 00:41:36 +0300

    Adjustments to Samsung Browser State Provider according to latest edits in the spreadsheet

--- src/browser-ui-state/state-providers/samsung-browser-state-provider.js
+++ src/browser-ui-state/state-providers/samsung-browser-state-provider.js
@@ -1,16 +1,32 @@
 import StateProvider from './state-provider'
+import DeviceDetector, {Devices} from "../device-detectors/device-detector"
 
 export default class SamsungBrowserStateProvider extends StateProvider {
-    constructor(screenObj, windowObj) {
-        const thresholds = {
+    constructor(userAgentObj, screenObj, windowObj) {
+        let thresholds = {
             landscape : {
-                collapsed: 21.65,
+                collapsed: 19.2,
                 keyboard: 65.6,
             },
             portrait : {
-                collapsed: 10.1,
-                keyboard: 30.2
+                collapsed: 11.9,
+                keyboard: 33.8
             }
+        }
+
+        const deviceDetector = new DeviceDetector(userAgentObj)
+
+        switch (deviceDetector.device) {
+            case Devices.SAMSUNG_GALAXY_NOTE_EDGE : thresholds = {
+                landscape : {
+                    collapsed: 12.4,
+                    keyboard: 38.0,
+                },
+                portrait : {
+                    collapsed: 13.7,
+                    keyboard: 30.7
+                }
+            }; break
         }
 
         super(screenObj, windowObj, thresholds)
