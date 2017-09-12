bb8eb9f04214a3241f7ff023ae9c1934f8befdad
andriyha <andriy.halyasovskyy@playtech.com>
2017-09-12 17:46:43 +0300

    Adding support for UC en on Galaxy Note 5

--- src/browser-ui-state/state-providers/ucbrowser-en-android-state-provider.js
+++ src/browser-ui-state/state-providers/ucbrowser-en-android-state-provider.js
@@ -18,13 +18,14 @@ export default class UCBrowserENAndroidStateProvider extends StateProvider {
 
         switch (deviceDetector.device) {
             case Devices.LENOVO_A889 :
+            case Devices.SAMSUNG_GALAXY_NOTE_5 :
             case Devices.SAMSUNG_GALAXY_S4 : thresholds = {
                 landscape : {
-                    collapsed: 12.4,
+                    collapsed: 11.3,
                     keyboard: 38.0,
                 },
                 portrait : {
-                    collapsed: 13.7,
+                    collapsed: 12.85,
                     keyboard: 30.7
                 }
             }; break
