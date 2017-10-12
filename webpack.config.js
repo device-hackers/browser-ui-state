f2ed01a9cf44474422bb40b74ae00c8b6356fc73
Andriy Halyasovskyy <thebit.hao@gmail.com>
2017-10-12 22:24:23 +0300

    Adjusting configuration to fix incorrect release (patch)

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,7 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        library: 'BrowserUiState'
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
