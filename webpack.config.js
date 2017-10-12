750c339a473065d4cebcb28c5a95553524139e9e
Andriy Halyasovskyy <thebit.hao@gmail.com>
2017-10-12 20:28:00 +0300

    Adjusting webpack.config.js to fix incorrect 1.0.2 release (patch)

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        library: 'BrowserUiState'
     },
     module: {
         rules: [
