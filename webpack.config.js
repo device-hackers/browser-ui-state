f60eab2975a68fac1441fb5362bb30241fdf1e5d
Andriy Halyasovskyy <thebit.hao@gmail.com>
2017-10-16 22:12:25 +0300

    Trying to fix Cucumber in dependent swipe-up lib which don't like imports in browser-ui-state (patch)

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
-        library: 'bUiState',
+        library: 'BrowserUiState',
         libraryTarget: 'var'
     },
     module: {
